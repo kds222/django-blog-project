952bfa53c788fd7d1468ce8b3d8a7ee92592e2a2
kds222 <daunopia@gmail.com>
2026-01-10 15:57:21 +0300

    add base, home and about templates and Bootstrap-based layout

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+posts = [
+    {
+        "author": "John Doe",
+        "title": "Blog post 1",
+        "content": "Post content",
+        "date_posted": "July 4, 1776",
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog post 2",
+        "content": "Post content",
+        "date_posted": "July 4, 1776",
+    },
+]
+
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {"posts": posts, "title": "Home"}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
